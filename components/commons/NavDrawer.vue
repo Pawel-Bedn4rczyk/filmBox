aa04329e6fd0a1d2655d55c1f370eb83bcb80df1
Pawel-Bedn4rczyk <Pawel-Bednarczyk-lca@protonmail.com>
2021-02-26 08:07:09 +0100

    drawer fixes #2

--- components/commons/NavDrawer.vue
+++ components/commons/NavDrawer.vue
@@ -10,34 +10,41 @@
     clipped-left
     app
     color="var(--v-appBar-base)"
-    style="overflow: hidden"
   >
-    <logo v-if="isMobile" :in-nav-drawer="true" />
-    <div class="d-flex flex-column justify-between" style="height: 93%">
-      <v-row class="ma-0">
-        <v-list nav dense>
-          <v-list-item link>
-            <v-list-item-icon>
-              <v-icon class="primaryColor">mdi-plus</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>{{ $t('navDrawer.addFilm') }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-row>
-      <v-row
-        v-if="isMobile"
-        class="ma-0"
-        no-gutters
-        align="end"
-        justify="space-around"
-      >
-        <v-col cols="7">
-          <switch-lang />
-        </v-col>
-        <v-col cols="2">
-          <theme-switcher />
-        </v-col>
-      </v-row>
+    <div class="d-flex flex-column justify-between" style="height: 100%">
+      <div v-if="isMobile" style="height: 50px">
+        <logo :in-nav-drawer="true" />
+      </div>
+      <div style="height: 100%">
+        <v-row class="my-0 mx-2">
+          <v-list nav dense style="width: 100%; padding: 0">
+            <v-list-item link>
+              <v-list-item-icon>
+                <v-icon class="primaryColor">mdi-plus</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>{{
+                $t('navDrawer.addFilm')
+              }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-row>
+      </div>
+      <div style="height: 50px">
+        <v-row
+          v-if="isMobile"
+          class="ma-0 mb-3"
+          no-gutters
+          align="start"
+          justify="space-around"
+        >
+          <v-col cols="7">
+            <switch-lang />
+          </v-col>
+          <v-col cols="2">
+            <theme-switcher />
+          </v-col>
+        </v-row>
+      </div>
     </div>
   </v-navigation-drawer>
 </template>
@@ -49,3 +56,8 @@ export default class NavDrawer extends Vue {
   @Prop() isMobile!: boolean
 }
 </script>
+<style scoped>
+.v-navigation-drawer >>> .v-navigation-drawer__content {
+  /* overflow-y: hidden !important; */
+}
+</style>
